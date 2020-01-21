201f8844accb77a902ebf9d9d21e4d749e76dddd
Lopatovas <Lopatovas@gmail.com>
2020-01-21 18:45:13 +0200

    Added navigation and pages

---
+++ src/containers/Landing/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="container">
+          <p>Landing page</p>
+      </div>
+    );
+  }
+}
+
+export default Landing;
